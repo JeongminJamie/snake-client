74c2472a7b283ff6b5b4147327ebd6413c5452a2
JeongminJamie <sksrhcalsu@gmail.com>
2022-05-11 17:17:23 -0400

    move snake

--- client.js
+++ client.js
@@ -10,17 +10,18 @@ const connect = function () {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
-  conn.on("connect", () => {
-    console.log("Successfully connected to game server");
-  });
-
   conn.on("data", (data) => {
     console.log(data);
   });
 
-  conn.write("Name: CJM");
-  
-
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: CJM");
+    // setInterval(() => { conn.write("Move: up") }, 50);
+    // setInterval(() => { conn.write("Move: down") }, 100);
+    // setInterval(() => { conn.write("Move: left") }, 150);
+    // setInterval(() => { conn.write("Move: right") }, 200);
+  })
 
   return conn;
 };
